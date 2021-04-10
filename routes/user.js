c8392aa7c679793cd245c9133afc4ceaec5fc814
Florian <florian.simunek@gmail.com>
2021-04-10 13:45:37 +0200

    Added user routes

---
+++ routes/user.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+
+//user - get
+router.get('/',(req, res, next) => {
+
+});
+
+
+//login - post
+router.post('/login',(req, res, next) => {
+
+});
+
+//register - post
+router.post('/register',(req, res, next) => {
+
+});
+
+
+//me - get
+router.get('/me',(req, res, next) => {
+
+});
+
+
+//userId - get
+router.get('/:userId',(req, res, next) => {
+
+});
+
+//userId - put
+router.put('/:userId',(req, res, next) => {
+
+});
+
+//userId - delete
+router.delete('/:userId',(req, res, next) => {
+
+});
+
+module.exports = router;
