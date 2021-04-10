df153b189a1a5a099a48b72962d12fe005fe4ade
Florian <florian.simunek@gmail.com>
2021-04-10 13:52:08 +0200

    Added react routes

---
+++ routes/react.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+
+//react - post
+router.post('/',(req, res, next) => {
+
+});
+
+
+//react - get
+router.get('/',(req, res, next) => {
+
+});
+
+
+//reactId - get
+router.get('/:reactId',(req, res, next) => {
+
+});
+
+//reactId - put
+router.put('/:reactId',(req, res, next) => {
+
+});
+
+//reactId - delete
+router.delete('/:reactId',(req, res, next) => {
+
+});
