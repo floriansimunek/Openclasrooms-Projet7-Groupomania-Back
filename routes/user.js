d6c78078aba82b79df785a393bce04c96e1ebee7
Florian <florian.simunek@gmail.com>
2021-04-13 12:33:01 +0200

    Deleted errors in user routes

--- routes/user.js
+++ routes/user.js
@@ -7,9 +7,9 @@ const userCtrl = require('../controllers/user');
 router.get('/', userCtrl.getAllUsers); //user - get
 router.post('/login', userCtrl.loginUser); //login - post
 router.post('/register', userCtrl.createUser); //register - post
-router.get('/me',(req, res, next) => userCtrl.getUserProfile); //me - get
-router.get('/:userId',(req, res, next) => userCtrl.getUser); //userId - get
-router.put('/:userId',(req, res, next) => userCtrl.modifyUser); //userId - put
-router.delete('/:userId',(req, res, next) => userCtrl.deleteUser); //userId - delete
+router.get('/me', userCtrl.getUserProfile); //me - get
+router.get('/:userId', userCtrl.getUser); //userId - get
+router.put('/:userId', userCtrl.modifyUser); //userId - put
+router.delete('/:userId', userCtrl.deleteUser); //userId - delete
 
 module.exports = router;
