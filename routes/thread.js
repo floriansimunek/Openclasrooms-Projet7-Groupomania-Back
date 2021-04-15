64785b1793ed9cfabb200ed798ecb1aca7d6e161
Florian <florian.simunek@gmail.com>
2021-04-15 14:53:07 +0200

    Created getMessage system

--- routes/thread.js
+++ routes/thread.js
@@ -10,7 +10,7 @@ router.get('/:threadId', /* auth, */ threadCtrl.getThread); //threadId - get
 router.put('/:threadId', /* auth, */ threadCtrl.modifyThread); //threadId - put
 router.delete('/:threadId', /* auth, */ threadCtrl.deleteThread); //threadId - delete
 router.post('/:threadId/message', /* auth, */ threadCtrl.createMessage); //message - post
-router.get('/:threadId/message', /* auth, */ threadCtrl.getAllMessage); //message - get
+router.get('/:threadId/message', /* auth, */ threadCtrl.getAllMessages); //message - get
 router.get('/:threadId/message/:messageId', /* auth, */ threadCtrl.getMessage); //messageId - get
 router.put('/:threadId/message/:messageId', /* auth, */ threadCtrl.modifyMessage); //messageId - put
 router.delete('/:threadId/message/:messageId', /* auth, */ threadCtrl.deleteMessage); //messageId - delete
